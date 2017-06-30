5b0cbb663346e4b7c789e289d3bced4228348a34
Alexandre Boudnik <Alexandre_Boudnik@epam.com>
2017-06-30 13:20:48 +0300

    clean up

--- src/main/java/org/boudnik/acl/sample/Guard.java
+++
@@ -1,9 +0,0 @@
-package org.boudnik.acl.sample;
-
-/**
- * @author Alexandre_Boudnik
- * @since 06/22/17 14:15
- */
-public class Guard {
-
-}
--- src/main/java/org/boudnik/acl/sample/Matrix.java
+++
@@ -1,9 +0,0 @@
-package org.boudnik.acl.sample;
-
-/**
- * @author Alexandre_Boudnik
- * @since 06/22/17 16:28
- */
-public class Matrix {
-    
-}
